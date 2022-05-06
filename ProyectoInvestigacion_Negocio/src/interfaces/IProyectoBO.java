dacb9bc6a99689e6bdd2a5dbd10bff2d726e3b44
Antonio-dp <antonio.delpardo229161@potros.itson.edu.mx>
2022-05-06 14:18:20 -0700

    Se agrego frame Agregar Proyectos y se creo Proyecto BO

---
+++ ProyectoInvestigacion_Negocio/src/interfaces/IProyectoBO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import entidades.Proyecto;
+
+/**
+ *
+ * @author pc
+ */
+public interface IProyectoBO {
+    public boolean repetido(Proyecto proyecto);
+}
