e689bb40e5f879bbb4f3a05f7179a044bca50b76
LAPTOP-NBSH865U\jegav <jesus.valencia228213@potros.itson.edu.mx>
2022-05-06 14:16:17 -0700

    PublicacionesCongresoDAO terminado

---
+++ ProyectoInvestigacion_Dominio/src/dtos/PublicacionProyectoDTO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtos;
+
+import entidades.Proyecto;
+
+/**
+ *
+ * @author jegav
+ */
+public class PublicacionProyectoDTO {
+    private Proyecto proyecto;
+
+    public Proyecto getProyecto() {
+        return proyecto;
+    }
+
+    public void setProyecto(Proyecto proyecto) {
+        this.proyecto = proyecto;
+    }
+    
+}
--- ProyectoInvestigacion_Persistencia/src/implementaciones/PublicacionesCongresoDAO.java
+++ ProyectoInvestigacion_Persistencia/src/implementaciones/PublicacionesCongresoDAO.java
@@ -8,11 +8,14 @@ package implementaciones;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import dtos.AutorDTO;
+import dtos.PublicacionProyectoDTO;
 import entidades.Proyecto;
 import entidades.PublicacionCongreso;
 import interfaces.IConexionBD;
 import interfaces.IPublicacionesCongreso;
+import java.util.ArrayList;
 import java.util.List;
+import org.bson.Document;
 import org.bson.types.ObjectId;
 
 /**
@@ -42,32 +45,105 @@ public class PublicacionesCongresoDAO implements IPublicacionesCongreso{
 
     @Override
     public boolean actualizar(PublicacionCongreso publicacionCongreso) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Document filtro = new Document("_id", publicacionCongreso.getId());
+        Document cambios = new Document()
+                .append("numeroSecuencia", publicacionCongreso.getNumeroSecuencia())
+                .append("titulo", publicacionCongreso.getTitulo())
+                .append("idProyecto", publicacionCongreso.getIdProyecto())
+                .append("autores", publicacionCongreso.getAutores())
+                .append("nombreCongreso", publicacionCongreso.getNombreCongreso())
+                .append("fechaInicio", publicacionCongreso.getFechaInicio())
+                .append("fechaFin", publicacionCongreso.getFechaFin())
+                .append("lugarCelebracion", publicacionCongreso.getLugarCelebracion())
+                .append("pais", publicacionCongreso.getPais())
+                .append("editorial", publicacionCongreso.getEditorial());
+        this.getColeccion().updateOne(filtro, new Document("$set", cambios));
+        
+        return true;
     }
 
     @Override
     public boolean eliminar(ObjectId idPublicacionCongreso) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.getColeccion().deleteOne(new Document("_id", idPublicacionCongreso));
+        return true;
     }
 
     @Override
     public PublicacionCongreso consultar(ObjectId idPublicacionCongreso) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<PublicacionCongreso> publicacionCongreso = this.getColeccion().find(new Document("_id", idPublicacionCongreso)).into(new ArrayList());
+        if(publicacionCongreso.isEmpty()){
+            return null;
+        }
+        return publicacionCongreso.get(0);
     }
 
     @Override
     public List<PublicacionCongreso> consultarTodos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.getColeccion().find().into(new ArrayList());
     }
 
     @Override
     public Proyecto consultarProyecto(ObjectId idPublicacionCongreso) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Document> etapas = new ArrayList();
+        etapas.add(new Document("$match", new Document("_id", idPublicacionCongreso)));
+        //etapas.add(new Document("$unwind", new Document("path", "$supervisiones")));
+        etapas.add(
+                new Document("$lookup", 
+                        new Document()
+                        .append("from", "proyectos")
+                        .append("localField", "idProyecto")
+                        .append("foreignField", "_id")
+                        .append("as", "proyecto")
+        ));
+        etapas.add(new Document("$unwind", new Document("path", "$proyecto")));
+        etapas.add(new Document("$project", 
+                new Document()
+                .append("_id", 0)
+                .append("proyecto", 1)
+        ));
+        
+        List<PublicacionProyectoDTO> proyecto = baseDatos.getCollection("noDoctores", PublicacionProyectoDTO.class).aggregate(etapas).into(new ArrayList());
+
+       
+        if(proyecto.isEmpty()){
+            return null;
+        }
+        
+        return proyecto.get(0).getProyecto();
     }
 
     @Override
     public List<AutorDTO> consultarAutores(ObjectId idPublicacionCongreso) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Document> etapas = new ArrayList();
+        etapas.add(new Document("$match", new Document("_id", idPublicacionCongreso)));
+        etapas.add(new Document("$unwind", new Document("path", "$autores")));
+        etapas.add(
+                new Document("$lookup", 
+                        new Document()
+                        .append("from", "proyectos")
+                        .append("localField", "autores.idProfesor")
+                        .append("foreignField", "_id")
+                        .append("as", "autoresPublicacion")
+        ));
+        etapas.add(new Document("$unwind", new Document("path", "$autoresPublicacion")));
+        etapas.add(new Document("$project", 
+                new Document()
+                .append("_id", 0)
+                .append("idAutor", "$autoresPublicacion._id")
+                .append("nombreAutor", "$autoresPublicacion.nombre")
+                .append("apellidoPaternoAutor", "$autoresPublicacion.apellidoPaterno")
+                .append("telefonoAutor", "$autoresPublicacion.telefono")
+                .append("orden", "$autores.orden")
+        ));
+        
+        List<AutorDTO> autores = baseDatos.getCollection("publicacionesCongreso", AutorDTO.class).aggregate(etapas).into(new ArrayList());
+
+       
+        if(autores.isEmpty()){
+            return null;
+        }
+        
+        return autores;
     }
     
 }
